49bbc60e84d3f2c6300bdbd31b2f9ef72940d863
kpomer <pomerkevin@gmail.com>
2025-08-01 12:57:46 +0300

    added functionality for 's', 'c', and 'n' commands

--- main.py
+++ main.py
@@ -14,13 +14,15 @@ Commands = [("'e'", "Save and Exit the Game"),
             ("'h'", "Display Help Menu"),
             ("'a [playerName]'", "Add a Player"),
             ("'r [playerName]'", "Remove a Player"),
-            ("'s [playerName] [playerName] [scoreChange]'", "Update Score"),
-            ("'n'", "New Game")]
+            ("'c [playerName] [playerName] [scoreChange]'", "Change Score"),
+            ("'s'", "Scoreboard"),
+            ("'n'", "New Game - set scores back to 0")]
 
 ErrorMessages = {"InvalidCommand": "Invalid Command: '{p1}'.  Please try again or run 'h' for a list of Help Options",
                  "InvalidNumArgs": "Invalid Number of Arguments for command '{p1}'.  Expected {p2} and received {p3}",
-                 "InvalidPlayerName": "Cannot Add new Player '{p1}' because this name is invalid",
-                 "PlayerAlreadyExists": "Cannot Add new Player '{p1}' because another player with this name already exists."}
+                 "InvalidValue": "Invalid {p1} value: '{p2}'",
+                 "PlayerAlreadyExists": "Cannot Add new Player '{p1}' because another player with this name already exists.",
+                 "PlayerNotExists": "Cannot Remove Player '{p1}' because no Player exists with this name."}
 
 
 def main():
@@ -38,8 +40,20 @@ def main():
             # Help Options
             DisplayHelpOptions()
         elif(userInput.split(" ")[0] == "a"):
-            # Add User
-            AddUser(userInput)
+            # Add Player
+            AddPlayer(userInput)
+        elif(userInput.split(" ")[0] == "r"):
+            # Remove Player
+            RemovePlayer(userInput)
+        elif(userInput.split(" ")[0] == "c"):
+            # Change Score
+            ChangeScore(userInput)
+        elif(userInput == "s"):
+            # Scoreboard
+            DisplayScoreboard()
+        elif(userInput == "n"):
+            # New Game
+            NewGame()
         else:
             ReturnError("InvalidCommand",[userInput])
 
@@ -48,24 +62,87 @@ def DisplayHelpOptions():
     for c in Commands:
         print(c[0] + ": " + c[1])
 
-def AddUser(commandInput):
+def AddPlayer(commandInput):
+    commandComponents = commandInput.split(" ")
+    #Validations
+    if(len(commandComponents) not in [2,3]):
+        ReturnError("InvalidNumArgs", [commandComponents[0], "2 or 3", len(commandComponents)])
+    elif(commandComponents[1] == ""):
+        ReturnError("InvalidValue", ["Player Name", commandComponents[1]])
+    elif(len(commandComponents) == 3 and not(commandComponents[2].isnumeric())):
+        ReturnError("InvalidValue", ["Score", commandComponents[2]])
+    elif(commandComponents[1] in PlayerScores):
+        ReturnError("PlayerAlreadyExists", [commandComponents[1]])
+    else:
+        #Add Player
+        score = 0
+        if(len(commandComponents) == 3):
+            score = int(commandComponents[2])
+
+        PlayerScores[commandComponents[1]] = score
+        print(f"Player '{commandComponents[1]}' has been added with a current score of {PlayerScores[commandComponents[1]]}!")
+
+def RemovePlayer(commandInput):
     commandComponents = commandInput.split(" ")
     #Validations
     if(len(commandComponents) != 2):
         ReturnError("InvalidNumArgs", [commandComponents[0], 2, len(commandComponents)])
     elif(commandComponents[1] == ""):
-        ReturnError("InvalidPlayerName", [commandComponents[1]])
-    elif(commandComponents[1] in PlayerScores):
-        ReturnError("PlayerAlreadyExists", [commandComponents[1]])
+        ReturnError("InvalidValue", ["Player Name", commandComponents[1]])
+    elif(commandComponents[1] not in PlayerScores):
+        ReturnError("PlayerNotExists", [commandComponents[1]])
     else:
-        PlayerScores[commandComponents[1]] = 0
-        print(f"Player '{commandComponents[1]}' has been added!")
+        #Remove Player
+        PlayerScores.pop(commandComponents[1])
+        print(f"Player '{commandComponents[1]}' has been removed!")
+
+def ChangeScore(commandInput):
+    commandComponents = commandInput.split(" ")
+    #Validations
+    if(len(commandComponents) != 3):
+        ReturnError("InvalidNumArgs", [commandComponents[0], 3, len(commandComponents)])
+    elif(commandComponents[1] == ""):
+        ReturnError("InvalidValue", ["Player Name", commandComponents[1]])
+    elif(commandComponents[1] not in PlayerScores):
+        ReturnError("PlayerNotExists", [commandComponents[1]])
+    elif(not(commandComponents[2].isnumeric())):
+        ReturnError("InvalidValue", ["Score", commandComponents[2]])
+    else:
+        #Change Points
+        previousPoints = PlayerScores[commandComponents[1]]
+        PlayerScores[commandComponents[1]] = previousPoints + int(commandComponents[2])
+        print(f"Score for '{commandComponents[1]}' has been updated!  New score is {PlayerScores[commandComponents[1]]}")
+
+def DisplayScoreboard():
+    if(len(PlayerScores) == 0):
+        print("\nNo scores exist")
+        return 0
     
+    print("\nSCOREBOARD")
+    
+    place = 0
+    sorted_scores = sorted(PlayerScores.items(), key=lambda item: item[1], reverse=True)
+    for s in range(len(sorted_scores)):
+        if(s == 0 or sorted_scores[s][1] != sorted_scores[s-1][1]):
+            place = s+1
+        print(f"{place} - {sorted_scores[s][0]} ({sorted_scores[s][1]})")    
+
+def NewGame():
+    print("\n'New Game' has been selected")
+    yn_NewGame = input("\nAre you sure?  Run 'y' to continue, otherwise press enter: ")
+    if(yn_NewGame == "y"):
+        print("\nNew Game Started!\nAll player scores have been reset to 0")
+        for k in PlayerScores.keys():
+            PlayerScores[k] = 0
+    else:
+        print("\nCommand skipped.  No changes have been made")
+        return 0
+
 
 
 def ReturnError(msgKey, parameters):
     if(msgKey not in ErrorMessages):
-        print("\nInternal error")
+        print("\nInternal Error")
         raise Exception(f"No ErrorMessage exists with the key '{msgKey}'")
     elif(len(parameters) == 0):
         print(ErrorMessages[msgKey])
